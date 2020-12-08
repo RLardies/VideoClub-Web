e90eba9e40ada9863f26c3a3abf4c621228b40d6
elacaba <eva.lacaba@estudiante.uam.es>
2020-12-08 17:52:31 +0100

    clientesDistintos.sql hecho

--- sql/clientesDistintos.sql
+++ sql/clientesDistintos.sql
@@ -1,12 +1,15 @@
-create or replace function clientesDistintos(fecha integer, totalAmount integer)
-    returns integer
+--pruebas
+drop index index_year;
+create index index_year on orders(extract(year from orderdate));
+drop index index_month;
+create index index_month on orders(extract(month from orderdate));
 
-begin
-    return query
+--nuestro elegido
+drop index index_yearmonth;
+create index index_yearmonth on orders(extract(year from orderdate), extract(month from orderdate));
 
 
-
-end;
-$$  language plpgsql;
-
-select * from  clientesDistintos(19000, 320000);
+EXPLAIN
+select count(distinct customerid)
+from orders
+where totalamount > 100 and extract(year from orderdate) = 2015 and extract(month from orderdate)= 4
